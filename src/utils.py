f219dc42ba114172eab530376835e65c9a094453
Maria Fedorova <maria.i.fedorova@yandex.ru>
2024-08-06 23:18:51 +0300

    new files are added

---
+++ src/utils.py
@@ -0,0 +1,47 @@
+from config import config
+from settings import company_id
+from src.db_manager import DBManager
+from src.work_api_employers import HHApiEmployer
+from src.work_api_vacancies import HHApiVacancies
+from exceptions import ApiError, ConnectError
+
+
+def load_data_employers():
+    """
+    Функция для загрузки данных о компаниях с HeadHunter.
+    """
+    api_employers = HHApiEmployer()
+
+    get_employers = []
+    try:
+        get_employers = api_employers.get_data_employers(company_id)
+    except ApiError:
+        print("Ошибка получения данных с HeadHunter")
+    return get_employers
+
+
+def load_data_vacancies():
+    """
+    Функция для загрузки данных о вакансиях с HeadHunter.
+    """
+    api_vacancies = HHApiVacancies()
+    api = []
+    try:
+        api = api_vacancies.get_vacancies(company_id)
+    except ApiError:
+        print("Ошибка получения данных с HeadHunter")
+    return api
+
+
+def load_data_database():
+    """
+    Функция создания базы данных и таблиц, и заполнения таблиц данными.
+    """
+    db_manager = DBManager('database_hh', 'postgres', config())
+    try:
+        db_manager.create_database()
+    except ConnectError:
+        print("Ошибка подключения к базе данных")
+
+    db_manager.create_tables()
+    db_manager.save_data_to_database(load_data_employers(), load_data_vacancies())
